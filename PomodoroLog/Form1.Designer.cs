902ae70a36a581659b37eae4f697d4892e33b757
Aravind M <blogsooner@gmail.com>
2013-07-17 22:53:50 -0700

    First commit

---
+++ PomodoroLog/Form1.Designer.cs
@@ -0,0 +1,306 @@
+﻿namespace PomodoroLog
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnAddOldTask = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(316, 65);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(9, 39);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(205, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.tbxTask_KeyDown);
+            this.textBox1.KeyUp += new System.Windows.Forms.KeyEventHandler(this.tbxTask_KeyUp);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(433, 65);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Done";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(316, 120);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Open log file";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(35, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(246, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Commas are not allowed in task and project names";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Location = new System.Drawing.Point(38, 29);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(240, 129);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Enabled = false;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(11, 91);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(205, 21);
+            this.comboBox1.TabIndex = 2;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(11, 67);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(155, 17);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.Text = "Choose from previous tasks";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(11, 14);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(96, 17);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Start new task:";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.comboBox2);
+            this.groupBox2.Controls.Add(this.radioButton4);
+            this.groupBox2.Controls.Add(this.textBox2);
+            this.groupBox2.Controls.Add(this.radioButton3);
+            this.groupBox2.Location = new System.Drawing.Point(38, 164);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(240, 126);
+            this.groupBox2.TabIndex = 8;
+            this.groupBox2.TabStop = false;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.Enabled = false;
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(11, 99);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(203, 21);
+            this.comboBox2.TabIndex = 3;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(11, 71);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(167, 17);
+            this.radioButton4.TabIndex = 2;
+            this.radioButton4.Text = "Choose from previous projects";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            this.radioButton4.CheckedChanged += new System.EventHandler(this.radioButton4_CheckedChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(11, 44);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(203, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Checked = true;
+            this.radioButton3.Location = new System.Drawing.Point(11, 20);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(85, 17);
+            this.radioButton3.TabIndex = 0;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "New project:";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.CustomFormat = "yyyy.MM.dd HH:mm";
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateTimePicker1.Location = new System.Drawing.Point(47, 345);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 9;
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.CustomFormat = "yyyy.MM.dd HH:mm";
+            this.dateTimePicker2.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateTimePicker2.Location = new System.Drawing.Point(49, 409);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker2.TabIndex = 10;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(50, 325);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Start date time:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(53, 390);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(72, 13);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "End date time";
+            // 
+            // btnAddOldTask
+            // 
+            this.btnAddOldTask.Location = new System.Drawing.Point(316, 371);
+            this.btnAddOldTask.Name = "btnAddOldTask";
+            this.btnAddOldTask.Size = new System.Drawing.Size(75, 23);
+            this.btnAddOldTask.TabIndex = 13;
+            this.btnAddOldTask.Text = "Add old task";
+            this.btnAddOldTask.UseVisualStyleBackColor = true;
+            this.btnAddOldTask.Click += new System.EventHandler(this.btnAddOldTask_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(601, 532);
+            this.Controls.Add(this.btnAddOldTask);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dateTimePicker2);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnAddOldTask;
+    }
+}
+
---
+++ PomodoroLog/Form1.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace PomodoroLog
+{
+    public partial class Form1 : Form
+    {
+        private const string logFileName = "tasklog.csv";
+        HashSet<string> taskList = new HashSet<string>();
+        private const int pomodoroUnit = 25;
+        HashSet<string> projectList = new HashSet<string>();
+        string lastTask;
+        DateTime start;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            btnStartClick();
+        }
+
+        private void btnStartClick()
+        {
+            this.WindowState = FormWindowState.Minimized;
+            start = DateTime.Now;
+            timer1.Enabled = true;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            DateTime t = DateTime.Now;
+            TimeSpan ts = TimeSpan.FromSeconds((t - start).TotalSeconds);
+            string sMin = (ts.Minutes < 10)?"0"+ts.Minutes.ToString():ts.Minutes.ToString();
+            string sSec= (ts.Seconds < 10)?"0"+ts.Seconds.ToString():ts.Seconds.ToString();
+            this.Text = sMin + ":" + sSec;
+            if (ts.Minutes == pomodoroUnit)
+            {
+                this.WindowState = FormWindowState.Normal;
+                timer1.Enabled = false;
+                string fileName = AppDomain.CurrentDomain.BaseDirectory + "/"+logFileName;
+                using (StreamWriter sw = new StreamWriter(fileName,true))
+                {
+                    string task = textBox1.Text;
+                    string project = string.IsNullOrWhiteSpace(textBox2.Text)? "Default" : textBox2.Text;
+                    sw.WriteLine(task + "," + start.ToShortDateString() + "," + start.ToShortTimeString() + "," + t.ToShortDateString() + "," + t.ToShortTimeString()+","+ts.TotalMinutes.ToString()+","+project);
+                    textBox1.Text = string.Empty;
+                }
+                UpdateTaskAndProjectList();
+            }
+        }
+
+        private void UpdateTaskAndProjectList()
+        {
+            GetPreviousTaskList();
+            LoadPreviousTaskList();
+            GetPreviousProjectList();
+            LoadPreviousProjectList();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+            DateTime t = DateTime.Now;
+            TimeSpan ts = TimeSpan.FromSeconds((t - start).TotalSeconds);
+            //write to csv file
+            string fileName = AppDomain.CurrentDomain.BaseDirectory + "/" + logFileName;
+            using (StreamWriter sw = new StreamWriter(fileName,true))
+            {
+                string task = textBox1.Text;
+                string project = string.IsNullOrWhiteSpace(textBox2.Text) ? "Default" : textBox2.Text;
+                sw.WriteLine(task + "," + start.ToShortDateString() + "," + start.ToShortTimeString() + "," + t.ToShortDateString() + "," + t.ToShortTimeString() + "," + ts.TotalMinutes.ToString() + "," + project);
+                textBox1.Text = string.Empty;
+            }
+            UpdateTaskAndProjectList();
+        }
+
+        private void tbxTask_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                btnStartClick();
+            }
+        }
+
+        private void tbxTask_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Oemcomma)
+            {
+                textBox1.Text = textBox1.Text.Replace(",", string.Empty);
+                textBox1.SelectionStart = textBox1.Text.Length;
+                textBox1.SelectionLength = 0;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                System.Diagnostics.Process.Start(AppDomain.CurrentDomain.BaseDirectory + "/" + logFileName);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.GetBaseException().Message);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                UpdateTaskAndProjectList();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            
+        }
+
+        private void GetPreviousProjectList()
+        {
+            string fileName = AppDomain.CurrentDomain.BaseDirectory + "/" + logFileName;
+            using (StreamReader sr = new StreamReader(fileName, true))
+            {
+                while (sr.Peek() >= 0)
+                {
+                    string[] items = sr.ReadLine().Split(',');
+                    if (items.Length >= 6)
+                    {
+                        projectList.Add(items[6]);
+                    }
+                }
+            }
+        }
+
+        private void LoadPreviousProjectList()
+        {
+            comboBox2.Items.Clear();
+            string[] itemList = projectList.ToArray();
+            comboBox2.Items.AddRange(itemList);
+            comboBox2.SelectedIndex = 0;
+        }
+
+        private void GetPreviousTaskList()
+        {
+            string fileName = AppDomain.CurrentDomain.BaseDirectory + "/" + logFileName;
+            using (StreamReader sr = new StreamReader(fileName, true))
+            {
+                while (sr.Peek() >= 0)
+                {
+                    taskList.Add(sr.ReadLine().Split(',')[0]);
+                }
+            }
+        }
+
+        private void LoadPreviousTaskList()
+        {
+            comboBox1.Items.Clear();
+            string[] itemList = taskList.ToArray();
+            comboBox1.Items.AddRange(itemList);
+            comboBox1.SelectedIndex = 0;
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            textBox1.Enabled = false;
+            comboBox1.Enabled = true;
+            textBox1.Text = comboBox1.Items[comboBox1.SelectedIndex].ToString();
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            textBox1.Enabled = true;
+            comboBox1.Enabled = false;
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            textBox1.Text = comboBox1.Items[comboBox1.SelectedIndex].ToString();
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            textBox2.Enabled = true;
+            comboBox2.Enabled = false;
+        }
+
+        private void radioButton4_CheckedChanged(object sender, EventArgs e)
+        {
+            comboBox2.Enabled = true;
+            textBox2.Enabled = false;
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            textBox2.Text = comboBox2.Items[comboBox2.SelectedIndex].ToString();
+        }
+
+        private void btnAddOldTask_Click(object sender, EventArgs e)
+        {
+            string fileName = AppDomain.CurrentDomain.BaseDirectory + "/" + logFileName;
+            using (StreamWriter sw = new StreamWriter(fileName, true))
+            {
+                string task = textBox1.Text;
+                string project = string.IsNullOrWhiteSpace(textBox2.Text) ? "Default" : textBox2.Text;
+                TimeSpan ts = TimeSpan.FromSeconds((dateTimePicker2.Value.TimeOfDay - dateTimePicker1.Value.TimeOfDay).TotalSeconds);
+                sw.WriteLine(task + "," + dateTimePicker1.Value.ToShortDateString()+ "," + dateTimePicker1.Value.ToShortTimeString() + "," + dateTimePicker2.Value.ToShortDateString() + "," + dateTimePicker2.Value.ToShortTimeString() + "," + ts.TotalMinutes.ToString() + "," + project);
+                textBox1.Text = string.Empty;
+            }
+            UpdateTaskAndProjectList();
+        }
+
+        
+    }
+}
